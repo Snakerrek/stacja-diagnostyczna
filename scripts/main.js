04aed3946e72b30fcf2ee2605fe17b3e59994537
Snakerrek <snakerrek@gmail.com>
2021-03-01 20:33:15 +0100

    Adding anchors and links to them. Writing smooth scroll handler.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
+
 const handleNavToggle = () => {
   const links = document.getElementsByClassName('link');
 
